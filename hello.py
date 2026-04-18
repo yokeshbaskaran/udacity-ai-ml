31e30d9d354536d1e2514c86cbc68d69c6e9449c
Yokesh <yokeshbaskaran@gmail.com>
2026-04-18 19:43:21 +0530

    error

--- hello.py
+++ hello.py
@@ -3,3 +3,15 @@ print("Hello Python!")
 
 x = 10
 print("The value of x is", x)
+
+
+x = 10
+for i in range:
+    x += 5
+print("value of i is :", i)
+
+# Error:
+#     Traceback (most recent call last):
+#   File "d:\Project\2026\python\udacity\tempCodeRunnerFile.py", line 2, in <module>
+#     for i in range:
+# TypeError: 'type' object is not iterable
