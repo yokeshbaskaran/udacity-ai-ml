8d895a2e9064381e49c2b22b566661a85b8acde0
Yokesh <yokeshbaskaran@gmail.com>
2026-04-18 19:47:36 +0530

    range cmds

--- hello.py
+++ hello.py
@@ -6,12 +6,18 @@ print("The value of x is", x)
 
 
 x = 10
-for i in range:
-    x += 5
-print("value of i is :", i)
+for i in range(10):
+    # x += 5
+    print("value of i is :", i)
 
 # Error:
 #     Traceback (most recent call last):
 #   File "d:\Project\2026\python\udacity\tempCodeRunnerFile.py", line 2, in <module>
 #     for i in range:
 # TypeError: 'type' object is not iterable
+
+# After learning the steps:
+x = 10
+for i in range(20):
+    i += 1
+    print("value of i is :", i)
