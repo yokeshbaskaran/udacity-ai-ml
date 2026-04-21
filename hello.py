f56c41578d22d55fb73cc0229af676f4417a0574
Yokesh <yokeshbaskaran@gmail.com>
2026-04-21 21:37:28 +0530

    removed

--- hello.py
+++ hello.py
@@ -5,17 +5,6 @@ x = 10
 print("The value of x is", x)
 
 
-x = 10
-for i in range(10):
-    # x += 5
-    print("value of i is :", i)
-
-# Error:
-#     Traceback (most recent call last):
-#   File "d:\Project\2026\python\udacity\tempCodeRunnerFile.py", line 2, in <module>
-#     for i in range:
-# TypeError: 'type' object is not iterable
-
 # After learning the steps:
 x = [1, 2, 3]
 for i in x:
